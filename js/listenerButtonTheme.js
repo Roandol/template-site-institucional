61cd0468abe12ebe083d31b201db7f6fdda8a6a1
Roan Lopes <roan.lopes10@hotmail.com>
2023-08-21 14:48:36 -0300

    Adicionando nova sections e adicionando transition

--- js/listenerButtonTheme.js
+++ js/listenerButtonTheme.js
@@ -1,12 +1,19 @@
 function listenerButtonTheme() {
     const buttonToggle = document.getElementById("themeToggle");
+    const imgToggle = buttonToggle.children[0];
 
     buttonToggle.addEventListener("click", toggleTheme)
 
     function toggleTheme() {
         let isDarkMode = window.__theme === "dark";
+        let modeSelected = isDarkMode ? "light" : "dark";
 
-        window.__setPreferredTheme(isDarkMode ? "light" : "dark");
+        toggleImg(modeSelected);
+        window.__setPreferredTheme(modeSelected);
+    }
+
+    function toggleImg(mode) {
+        imgToggle.src = `images/${mode}-mode.png`;
     }
 }
 
